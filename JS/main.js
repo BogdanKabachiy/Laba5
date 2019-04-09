c1fb596b2269f03de3ba8f0c0e6a70a7440c0fb5
BogdanKabachiy <kabachiy1@gmail.com>
2019-04-09 19:54:16 +0300

    5commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+try{
+    for(var i =1; i>0; i++)
+    {
+    if(i >= 10000)
+        throw Error("Nice job! It`s an Error in your code");
+    console.log(i);
+    }
+}
+catch(poimal)
+{
+    console.log(poimal);
+}   
